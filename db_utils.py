c1cf18450678daef87418fa8fc24a82d4166c5d5
Sumit <sumitk880005@gmail.com>
2026-04-15 13:52:18 +0530

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -2,7 +2,8 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 
-DB_PATH = 'database/reviews.db'
+DB_PATH = "database.db"
+
 
 def get_conn():
     return sqlite3.connect(DB_PATH, check_same_thread=False)
